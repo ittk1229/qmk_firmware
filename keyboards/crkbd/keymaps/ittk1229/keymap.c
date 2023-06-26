8b70d2183eb50978daee512faedeb8c77a0a4778
ittk1229 <s2010485@u.tsukuba.ac.jp>
2023-06-26 14:15:49 +0900

    Delete minecraft layer and move num row to home row

--- keyboards/crkbd/keymaps/ittk1229/keymap.c
+++ keyboards/crkbd/keymaps/ittk1229/keymap.c
@@ -9,7 +9,6 @@ enum layers {
   _LOWER,
   _RAISE,
   _ADJUST,
-  _MINECRAFT
 };
 
 // マクロキーの定義
@@ -29,7 +28,6 @@ enum custom_keycodes {
 #define HOME_N SFT_T(KC_N)
 #define HOME_S ALT_T(KC_S)
 #define HOME_B GUI_T(KC_B)
-#define TG_MC TG(_MINECRAFT)
 
 // レイヤーごとにキーマップを定義
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                        KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_NO,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_NO,   KC_NO,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                        KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -71,25 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,  KC_F11,  KC_F12,   KC_NO,   KC_NO,   KC_NO,                        KC_NO, RGB_TOG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG_MC,\
+        KC_NO,  KC_F11,  KC_F12,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             KC_NO,   LOWER,   KC_NO,      KC_NO,   RAISE,   KC_NO\
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [_MINECRAFT] = LAYOUT( \
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,   KC_NO,    KC_Q,    KC_W,    KC_E,    KC_T,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,   KC_NO,    KC_A,    KC_S,    KC_D,    KC_F,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      JP_SLSH,   KC_F1,   KC_F2,   KC_F3,   KC_F5,  KC_F11,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG_MC,\
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, KC_LSFT,  KC_SPC,      KC_NO,   KC_NO,   KC_NO\
                                       //`--------------------------'  `--------------------------'
   ),
 };
@@ -146,7 +132,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #endif //RGBLIGHT_ENABLE
 
 
-// lower/raiseのime切り替えに利用
+// lower/raise の ime 切り替えに利用
 static bool lower_pressed = false;
 static bool raise_pressed = false;
 
@@ -267,9 +253,6 @@ void oled_render_layer_state(void) {
       break;
     case _ADJUST:
       oled_write_ln_P(PSTR("Adjust"), false);
-      break;
-    case _MINECRAFT:
-      oled_write_ln_P(PSTR("Minecraft"), false);
       break;
     default:
       oled_write_ln_P(PSTR("Undefined"), false);
