705469dc05946af4104290b6c6e16dc0bc03a800
ittk1229 <s2010485@u.tsukuba.ac.jp>
2023-07-09 12:04:22 +0900

    Add Shift and minus key to NAGINATA layer

--- keyboards/crkbd/keymaps/ittk1229/keymap.c
+++ keyboards/crkbd/keymaps/ittk1229/keymap.c
@@ -48,13 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAGINATA] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     ESC_MHEN,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_NO,\
+     ESC_MHEN,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, JP_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        TO_EUC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, JP_COMM,  JP_DOT, JP_SLSH,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_BSPC\
+                                          KC_LSFT,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_BSPC\
                                       //`--------------------------'  `--------------------------'
   ),
 
